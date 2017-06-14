bbc91abf1dbf9784c6fa459c2c93cba8fd495bcf
crystall24 <aganina201424@mail.ru>
2017-06-14 19:48:59 +0700

    add gson gradle dependency

---
+++ app/src/main/java/com/example/translator/api/TranslateApiService.java
@@ -0,0 +1,8 @@
+package com.example.translator.api;
+
+/**
+ * Created by Алекс on 14.06.2017.
+ */
+
+public interface TranslateApiService {
+}
---
+++ app/src/main/java/com/example/translator/model/TranslateItem.java
@@ -0,0 +1,8 @@
+package com.example.translator.model;
+
+/**
+ * Created by Алекс on 14.06.2017.
+ */
+
+class TranslateItem {
+}
