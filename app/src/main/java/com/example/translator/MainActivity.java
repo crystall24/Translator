bd4efc6f7113c00624e5c73fe4c3ba22f7879b97
crystall24 <aganina201424@mail.ru>
2017-06-16 15:53:50 +0700

    output translation in log

--- app/src/main/java/com/example/translator/MainActivity.java
+++ app/src/main/java/com/example/translator/MainActivity.java
@@ -1,13 +1,77 @@
 package com.example.translator;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window.Callback;
+
+import com.example.translator.api.TranslateApiService;
+import com.example.translator.model.TranslateItem;
+
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        LoadTranslation trans = new LoadTranslation();
+
+        trans.execute();
+
+
+
+
+
         setContentView(R.layout.activity_main);
+
     }
+
+    private class LoadTranslation extends AsyncTask<Void, Void, TranslateItem > {
+        @Override
+        protected TranslateItem doInBackground(Void... params) {
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://translate.yandex.net/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            TranslateApiService service = retrofit.create(TranslateApiService.class);
+
+            Call<TranslateItem> translation = service.getTranslation();
+
+//            String item[] = null;
+            TranslateItem item = new TranslateItem();
+            try {
+                item = translation.execute().body();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+            return item;
+        }
+        /**@Override protected void onPreExecute() {
+        super.onPreExecute();
+        pDialog = new ProgressDialog(MainActivity.this);
+        pDialog.setMessage("");
+        pDialog.show();
+
+        }**/
+
+        @Override
+        protected void onPostExecute(TranslateItem tr){
+            Log.d("MAINACTIVITY",tr.getText()[0]);
+        }
+    }
+
 }
--- app/src/main/java/com/example/translator/model/TranslateItem.java
+++ app/src/main/java/com/example/translator/model/TranslateItem.java
@@ -1,8 +1,31 @@
 package com.example.translator.model;
 
-/**
- * Created by Алекс on 14.06.2017.
- */
+public class TranslateItem {
+    private int code;
+    private String[] text;
+    private String lang;
 
-class TranslateItem {
+    public int getCode() {
+        return this.code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String[] getText() {
+        return this.text;
+    }
+
+    public void setText(String[] text) {
+        this.text = text;
+    }
+
+    public String getLang() {
+        return this.lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
 }
