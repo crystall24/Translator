5dfe61df3df0641c3c367b027b52ac5950396220
crystall24 <aganina201424@mail.ru>
2017-07-20 15:41:51 +0700

    show translation on the screen

--- app/src/main/java/com/example/translator/MainActivity.java
+++ app/src/main/java/com/example/translator/MainActivity.java
@@ -1,38 +1,32 @@
 package com.example.translator;
-
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Window.Callback;
-
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import com.example.translator.api.TranslateApiService;
 import com.example.translator.model.TranslateItem;
-
-
 import java.io.IOException;
-
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.translated_text) TextView translatedText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         LoadTranslation trans = new LoadTranslation();
-
         trans.execute();
-
-
-
-
-
         setContentView(R.layout.activity_main);
-
+        ButterKnife.bind(MainActivity.this);
     }
 
     private class LoadTranslation extends AsyncTask<Void, Void, TranslateItem > {
@@ -47,17 +41,12 @@ public class MainActivity extends AppCompatActivity {
             TranslateApiService service = retrofit.create(TranslateApiService.class);
 
             Call<TranslateItem> translation = service.getTranslation();
-
-//            String item[] = null;
             TranslateItem item = new TranslateItem();
             try {
                 item = translation.execute().body();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-
-
             return item;
         }
         /**@Override protected void onPreExecute() {
@@ -69,7 +58,8 @@ public class MainActivity extends AppCompatActivity {
         }**/
 
         @Override
-        protected void onPostExecute(TranslateItem tr){
+        protected void onPostExecute(TranslateItem tr) {
+            translatedText.setText("translated: " + tr.getText()[0]);
             Log.d("MAINACTIVITY",tr.getText()[0]);
         }
     }
